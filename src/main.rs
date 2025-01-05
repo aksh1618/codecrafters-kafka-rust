291cf666fa27f79be04643c3849dc443b6449cb1
aksh1618 <aakarshituppal@gmail.com>
2025-01-06 02:46:01 +0530

    parse correlation id

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::{
-    io::{Result, Write},
+    io::{Read, Result, Write},
     net::{TcpListener, TcpStream},
 };
+
+use bytes::{Bytes, BytesMut};
 
 const ADDR: &str = "127.0.0.1:9092";
 
@@ -28,35 +30,104 @@ fn start_server() -> Result<()> {
 }
 
 fn handle_connection_response_v0(mut stream: TcpStream) -> Result<()> {
+    let request_correlation_id = read_correlation_id_request_v2(&mut stream)?;
     stream.write_all(0i32.to_be_bytes().as_slice())?;
-    stream.write_all(7i32.to_be_bytes().as_slice())?;
+    stream.write_all(request_correlation_id.as_ref())?;
     stream.flush()?;
     Ok(())
 }
 
+fn read_correlation_id_request_v2(stream: &mut TcpStream) -> Result<Bytes> {
+    // 12 bytes: 4 message size | 2 api key | 2 api version | 4 correlation id
+    // See: https://kafka.apache.org/protocol.html
+    let mut buf = BytesMut::zeroed(12);
+    stream.read_exact(&mut buf)?;
+    println!("read 12 bytes");
+    let remaining_bytes_read = consume_bytes(stream)?;
+    println!("read remaining {remaining_bytes_read} bytes");
+    return Ok(buf.freeze().slice(8..));
+}
+
+fn consume_bytes(stream: &mut TcpStream) -> Result<usize> {
+    let mut buf = vec![];
+    let mut total_bytes_read = 0;
+    loop {
+        let n = stream.read(&mut buf)?;
+        total_bytes_read += n;
+        if n == 0 {
+            break;
+        }
+    }
+    Ok(total_bytes_read)
+}
+
 #[cfg(test)]
 mod tests {
+    use indoc::indoc;
+
+    use super::*;
+    use std::sync::Once;
     use std::{
         io::{Read, Result},
+        process::Command,
         thread,
     };
 
-    use super::*;
+    static INIT: Once = Once::new();
+
+    pub fn ensure_server_running() {
+        INIT.call_once(|| {
+            thread::spawn(|| {
+                start_server().unwrap_or_else(|e| {
+                    panic!("Should be able to start the server on {ADDR}: {e}")
+                });
+            });
+        });
+    }
 
     #[test]
-    fn test_response_v0_header() -> Result<()> {
-        thread::spawn(|| {
-            start_server()
-                .unwrap_or_else(|_| panic!("Should be able to start the server on {ADDR}"));
-        });
-        let stream = std::net::TcpStream::connect(ADDR)?;
+    fn test_cmd_request_v2_response_v0_correlation_id() -> Result<()> {
+        ensure_server_running();
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("echo -n '00000023001200046f7fc66100096b61666b612d636c69000a6b61666b612d636c6904302e3100' | xxd -r -p | nc localhost 9092 | hexdump -C")
+            .output()?
+            .stdout;
+        let output = String::from_utf8_lossy(&output);
+
+        assert_eq!(
+            output,
+            indoc! {"
+             00000000  00 00 00 00 6f 7f c6 61                           |....o..a|
+             00000008
+            "}
+        );
+        println!("{output:?}");
+        Ok(())
+    }
+
+    #[test]
+    fn test_request_v2_response_v0_correlation_id() -> Result<()> {
+        #[allow(clippy::unreadable_literal)]
+        // const CORRELATION_ID: i32 = 1870644833i32;
+        const CORRELATION_ID: i32 = 1234567890i32;
+        ensure_server_running();
+        let mut stream = std::net::TcpStream::connect(ADDR)?;
+        stream.write_all(0i32.to_be_bytes().as_slice())?;
+        stream.write_all(0i16.to_be_bytes().as_slice())?;
+        stream.write_all(0i16.to_be_bytes().as_slice())?;
+        stream.write_all(CORRELATION_ID.to_be_bytes().as_slice())?;
+        stream.write_all((-1_i16).to_be_bytes().as_slice())?;
+        stream.write_all(0i8.to_be_bytes().as_slice())?;
+        stream.flush()?;
+        println!("Wrote and flushed all bytes");
         let mut res_bytes = stream.bytes();
         let message_size_bytes = 0i32.to_be_bytes();
         for byte in message_size_bytes {
             println!("Received byte: {byte}");
             assert!(matches!(res_bytes.next(), Some(Ok(res_byte)) if res_byte == byte));
         }
-        let header_bytes = 7i32.to_be_bytes();
+        let header_bytes = CORRELATION_ID.to_be_bytes();
         for byte in header_bytes {
             println!("Received byte: {byte}");
             assert!(matches!(res_bytes.next(), Some(Ok(res_byte)) if res_byte == byte));
