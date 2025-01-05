53f510a06e1e170a7107ba53d73d34a9c4ace228
aksh1618 <aakarshituppal@gmail.com>
2025-01-06 00:13:07 +0530

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,66 @@
-#![allow(unused_imports)]
 use std::{
     io::{Result, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
+
+const ADDR: &str = "127.0.0.1:9092";
 
 fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+    start_server()
+}
 
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-
+fn start_server() -> Result<()> {
+    let listener = TcpListener::bind(ADDR)?;
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                _stream.write_all(0i32.to_be_bytes().as_slice()).unwrap();
-                _stream.write_all(7i32.to_be_bytes().as_slice()).unwrap();
-                _stream.flush()?;
+                handle_connection_response_v0(stream)?;
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error: {e}");
             }
         }
     }
     Ok(())
 }
 
+fn handle_connection_response_v0(mut stream: TcpStream) -> Result<()> {
+    stream.write_all(0i32.to_be_bytes().as_slice())?;
+    stream.write_all(7i32.to_be_bytes().as_slice())?;
+    stream.flush()?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use std::io::Read;
+    use std::{
+        io::{Read, Result},
+        thread,
+    };
 
     use super::*;
 
     #[test]
-    fn test_response_v0_header() {
-        let stream = std::net::TcpStream::connect("127.0.0.1:9092")
-            .expect("The program should be running on port 9092 before the test is run");
+    fn test_response_v0_header() -> Result<()> {
+        thread::spawn(|| {
+            start_server()
+                .unwrap_or_else(|_| panic!("Should be able to start the server on {ADDR}"));
+        });
+        let stream = std::net::TcpStream::connect(ADDR)?;
         let mut res_bytes = stream.bytes();
         let message_size_bytes = 0i32.to_be_bytes();
         for byte in message_size_bytes {
-            println!("Received byte: {}", byte);
+            println!("Received byte: {byte}");
             assert!(matches!(res_bytes.next(), Some(Ok(res_byte)) if res_byte == byte));
         }
         let header_bytes = 7i32.to_be_bytes();
         for byte in header_bytes {
-            println!("Received byte: {}", byte);
+            println!("Received byte: {byte}");
             assert!(matches!(res_bytes.next(), Some(Ok(res_byte)) if res_byte == byte));
         }
+        Ok(())
     }
 }
