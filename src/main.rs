f9393cadf30d1099cf0ca36e9e4f3bb17ff3f489
aksh1618 <aakarshituppal@gmail.com>
2025-01-05 21:05:37 +0530

    bind to a port
    out of syllabus: added printing of the request as well

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, Read},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                let f = BufReader::new(_stream);
+                println!("accepted new connection");
+                println!("Received request:\n");
+                let bytes = f.bytes().collect::<Result<Vec<_>, _>>().unwrap();
+                print!("{}", String::from_utf8(bytes).unwrap());
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
